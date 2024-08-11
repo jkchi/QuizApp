9163269692d731c6e5e30ce6d7910ff8b62ce320
jkchi <113892417+jkchi@users.noreply.github.com>
2024-08-10 20:53:57 -0400

    create empty submission after new user register need test

---
+++ backend/accounts/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from submissions.models import Submission
+from quizzes.models import Quiz
+
+User = get_user_model()
+
+# create empty submission for new registered user
+# when some quizzes are published
+@receiver(post_save, sender=User)
+def create_submissions_for_new_user(sender, instance, created, **kwargs):
+    if created and not instance.is_staff:
+        quizzes = Quiz.objects.filter(is_published=True)
+
+        submissions = [
+            Submission(
+                quiz=quiz,
+                student=instance,
+                score=0,
+                attendance_status=False,
+                submitted_at=None
+            )
+            for quiz in quizzes
+        ]
+        
+        Submission.objects.bulk_create(submissions)
